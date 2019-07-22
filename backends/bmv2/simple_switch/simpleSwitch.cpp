83532d196819a553393ef4063e298933226565a9
andreyqg <51326707+andreyqg@users.noreply.github.com>
2019-07-22 14:42:05 -0300

    Add support for count sketch hashing

--- backends/bmv2/simple_switch/simpleSwitch.cpp
+++ backends/bmv2/simple_switch/simpleSwitch.cpp
@@ -193,12 +193,12 @@ Util::IJson* ExternConverter_hash::convertExternFunction(
         v1model.algorithm.h3.name, v1model.algorithm.h4.name,
         v1model.algorithm.g1.name, v1model.algorithm.g2.name,
         v1model.algorithm.g3.name, v1model.algorithm.g4.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd1.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd2.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd3.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd4.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd5.name,
-        v1model.algorithm.g3.name, v1model.algorithm.hhd6.name
+        v1model.algorithm.g3.name, v1model.algorithm.d1.name,
+        v1model.algorithm.g3.name, v1model.algorithm.d2.name,
+        v1model.algorithm.g3.name, v1model.algorithm.d3.name,
+        v1model.algorithm.g3.name, v1model.algorithm.d4.name,
+        v1model.algorithm.g3.name, v1model.algorithm.d5.name,
+        v1model.algorithm.g3.name, v1model.algorithm.d6.name
     };
 
     if (mc->arguments->size() != 5) {
